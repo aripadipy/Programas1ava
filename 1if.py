d9171cae4f71969a04e828261cf047fff5e5976a
aripadipy <71645363+aripadipy@users.noreply.github.com>
2020-10-06 13:15:51 +0200

    if

---
+++ 1if.py
@@ -0,0 +1,8 @@
+nombre1=int(input("Primer nombre"))
+nombre2=int(input("Segon nombre"))
+if nombre1>nombre2:
+  print("El nombre1> que el nombre2")
+elif nombre1==nombre2:
+  print("Els dos nombres son igual")
+else:
+  print("nombre1<nombre2")
