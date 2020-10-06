dc40809fbf6aa3a7be1f41eef0f9e94162cb5d40
aripadipy <71645363+aripadipy@users.noreply.github.com>
2020-10-06 12:49:35 +0200

    uu

---
+++ usuari.py
@@ -0,0 +1,4 @@
+import random
+nom=input("Escriu el teu nom:")
+cognom=input("Escriu el teu primer cognom:")
+print("El teu usuari es:",nom,".",cognom,"la teva contrasenya es:",random.randint(100,999))
