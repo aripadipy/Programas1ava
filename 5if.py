a71b84a2ef6bd8e4a68d7642115c781f8e90c2ed
aripadipy <71645363+aripadipy@users.noreply.github.com>
2020-10-13 12:57:21 +0200

    Add files via upload

---
+++ 5if.py
@@ -0,0 +1,12 @@
+var=input("Escriu un simbol")
+if var=="0":print("hola")
+if var=="1": print("hola")
+if var=="2":print("hola")
+if var=="3":print("hola")
+if var=="4":print("hola")
+if var=="5":print("hola")
+if var=="6":print("hola")
+if var=="7":print("hola")
+if var=="8":print("hola")
+if var=="9":print("hola")
+
