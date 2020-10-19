d9df47f20a41cfbd77f72b6d7301a3cb6c934d0e
aripadipy <71645363+aripadipy@users.noreply.github.com>
2020-10-19 14:00:37 +0200

    Add files via upload

---
+++ 8if.py
@@ -0,0 +1,9 @@
+dia=int(input("Dime el dia"))
+mes=int(input("Dime el mes"))
+año=int(input("Dime el año"))
+if dia>30:
+	print("Només hi han 30 dias")
+if mes>12:
+	print("Hi han 12 mesos")
+if año>2020:
+	print("Aún no hemos llegado a ese año")
